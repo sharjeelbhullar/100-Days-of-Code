ab4352a8f9bf267d7c3d4d62bc4340e2c815c1f5
sharjeelbhullar <sharjeelbhullar@gmail.com>
2024-10-09 23:35:27 +0500

    Add function with parameters

--- day-6.py
+++ day-6.py
@@ -1,5 +1,15 @@
+#Simple function without parameter/argument
 def greetings():
     name = input("Enter your name please: ")
     print(f"Hello {name}")
 
 greetings()
+
+#Function with parameters
+def full_name(first_name, last_name):
+    print(first_name + " " + last_name)
+
+first_name = input("Enter your first name : ")
+sur_name = input("Enter your sur name: ")
+full_name(first_name, sur_name)
+
